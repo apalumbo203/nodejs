c88c2cd2e711d5d4af23d21fbd046e727effff91
Adrien Palumbo <adrienpalumbo203@gmail.com>
2017-12-29 23:32:18 +0100

    FEAT#001: WIP on url query ... @0h

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
 #!/usr/bin/env node
 
-var http = require('http');
+var g_http = require('http');
+var g_url = require('url');
+var g_query = require('querystring');
 
-var server = http.createServer(function(req, res) {
-    var l_string = "";
+var g_http_mime = {"Content-Type": "text/html"};
 
-    l_string += '<h1> Hello World ! </h1>';
-    l_string += '<p>' + req.toString() + '</p>';
+var g_html_index = '<!DOCTYPE html>'+
+    '<html>'+
+    '    <head>'+
+    '        <meta charset="utf-8" />'+
+    '        <title>Ma page Node.js !</title>'+
+    '    </head>'+
+    '    <body>'+
+    '     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
+    '    </body>'+
+    '</html>';
 
-    res.writeHead(200);
-    res.end(l_string);
-});
+var server_client_handler = function(req, res) {
+    var l_page_path = g_url.parse(req.url).pathname;
+    var l_query = g_url.parse(req.url).query;
 
-server.listen(8080);
+    console.log(l_page_path);
+
+    if (l_query != null)
+    {
+        var l_hq = g_query.parse(l_query);
+
+        console.log("\t" + l_hq['username']);
+    }
+
+
+    res.writeHead(200, g_http_mime);
+    res.end(g_html_index);
+};
+
+var server_main = function()
+{
+    var l_server = g_http.createServer(server_client_handler);
+
+    l_server.listen(8080);
+};
+
+server_main();
